1a135dff9f8f496b2350852671111bd37b5d26c3
Fernas like franjudinhas <162641282+FERNAS777@users.noreply.github.com>
2024-10-22 20:25:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,339 @@
+Pular para o conteúdo principal
+Google Sala de Aula
+Google Sala de Aula
+Matematica Ii - 3º Ano C Noite - Médio
+JOSE GOMES DO AMARAL, C E PROF-EFM
+Início
+Agenda
+Recursos
+Novo
+Minhas inscrições
+Pendentes
+M
+Matematica - 3º Ano C Noite - Médio
+JOSE GOMES DO AMARAL, C E PROF-EFM
+M
+Matematica Ii - 3º Ano C Noite - Médio
+JOSE GOMES DO AMARAL, C E PROF-EFM
+3
+3° C - Matematica Iii - 3º Ano C Noite - Médio
+JOSE GOMES DO AMARAL, C E PROF-EFM
+F
+Fisica Iii - 3º Ano C Noite - Médio
+JOSE GOMES DO AMARAL, C E PROF-EFM
+E
+Educacao Fisica - 3º Ano C Noite - Médio
+JOSE GOMES DO AMARAL, C E PROF-EFM
+F
+Fisica - 3º Ano C Noite - Médio
+JOSE GOMES DO AMARAL, C E PROF-EFM
+F
+Fisica Ii - 3º Ano C Noite - Médio
+JOSE GOMES DO AMARAL, C E PROF-EFM
+E
+Educacao Financeira - 3º Ano C Noite - Médio
+JOSE GOMES DO AMARAL, C E PROF-EFM
+B
+Biologia Ii - 3º Ano C Noite - Médio
+JOSE GOMES DO AMARAL, C E PROF-EFM
+Q
+Quimica I - 3º Ano C Noite - Médio
+JOSE GOMES DO AMARAL, C E PROF-EFM
+3
+3º C - Lingua Portuguesa - Noite - Médio
+JOSE GOMES DO AMARAL, C E PROF-EFM
+P
+Projeto De Vida - 3º Ano C Noite - Médio
+JOSE GOMES DO AMARAL, C E PROF-EFM
+Turmas arquivadas
+Configurações
+Mural
+Atividades
+Pessoas
+Mural
+Matematica Ii - 3º Ano C Noite - Médio
+JOSE GOMES DO AMARAL, C E PROF-EFM
+Próximas atividades
+Nenhuma atividade para a próxima semana!
 
+Escreva um aviso para sua turma
+
+Aviso: "Aula 6 Técnicas computacionais:…"
+ROBERTO BATISTA CONRADO
+Criado em: 19:5419:54
+Aula 6 
+Técnicas computacionais: refletindo sobre Inteligência Artificial na escola.
+
+Login | Alura - Cursos online de tecnologia
+https://cursos.alura.com.br/course/tecnicas-computacionais-IA-na-escola/task/148630?authuser=0
+
+Adicionar comentário para a turma...
+
+Atividade: "Aula assíncrona 21/10/2024."
+ROBERTO BATISTA CONRADO postou uma nova atividade: Aula assíncrona 21/10/2024.
+Criado em: OntemOntem
+Atividade: "Aula assíncrona 15/10/2024."
+ROBERTO BATISTA CONRADO postou uma nova atividade: Aula assíncrona 15/10/2024.
+Criado em: 15 de out.15 de out.
+Atividade: "Aula assíncrona 07/10/2024."
+ROBERTO BATISTA CONRADO postou uma nova atividade: Aula assíncrona 07/10/2024.
+Criado em: 7 de out.7 de out.
+
+Aviso: "Aula assíncrona 30/09/2024. -Criando um…"
+ROBERTO BATISTA CONRADO
+Criado em: 30 de set.30 de set. (editado: 2 de out.)
+Aula assíncrona 30/09/2024.
+-Criando um repositório do GitHub.
+-Criando arquivos no GitHub.
+-Como Abrir seu repositório no VScode online.
+-Como usar o Vercel no seu repositório do GitHub.
+
+Como criar um repositório no Github?
+Vídeo do YouTube • ‪5 minutos‬
+
+Abrindo o repositorio direto no VScode online.pdf
+PDF
+
+Criando arquivos no GitHub.pdf
+PDF
+
+Como Hospedar Seu Projeto Online: Tutorial Completo com Vercel e GitHub
+Vídeo do YouTube • ‪7 minutos‬
+
+Adicionar comentário para a turma...
+
+
+Aviso: "24/09/2024. Projeto "Técnicas…"
+ROBERTO BATISTA CONRADO
+Criado em: 23 de set.23 de set.
+24/09/2024.
+Projeto  "Técnicas computacionais: refletindo sobre Inteligência Artificial na escola".
+Aula 05. Corrigindo problemas.
+
+Login | Alura - Cursos online de tecnologia
+https://cursos.alura.com.br/course/tecnicas-computacionais-IA-na-escola/task/148629
+
+Adicionar comentário para a turma...
+
+Atividade: "Aula assíncrona 23/09/2024."
+ROBERTO BATISTA CONRADO postou uma nova atividade: Aula assíncrona 23/09/2024.
+Criado em: 23 de set.23 de set.
+
+Aviso: "17/09/2024. Projeto "Técnicas…"
+ROBERTO BATISTA CONRADO
+Criado em: 17 de set.17 de set.
+17/09/2024.
+Projeto  "Técnicas computacionais: refletindo sobre Inteligência Artificial na escola".
+Aula 04. Botão de jogar novamente.
+
+Login | Alura - Cursos online de tecnologia
+https://cursos.alura.com.br/course/tecnicas-computacionais-IA-na-escola/task/148628
+
+Adicionar comentário para a turma...
+
+Atividade: "Aula assíncrona 16/09/2024."
+ROBERTO BATISTA CONRADO postou uma nova atividade: Aula assíncrona 16/09/2024.
+Criado em: 16 de set.16 de set.
+
+Aviso: "10/09/2024. Projeto "Técnicas…"
+ROBERTO BATISTA CONRADO
+Criado em: 10 de set.10 de set. (editado: 10 de set.)
+10/09/2024.
+Projeto  "Técnicas computacionais: refletindo sobre Inteligência Artificial na escola".
+Aula 03. Divisão eficiente de códigos.
+
+Login | Alura - Cursos online de tecnologia
+https://cursos.alura.com.br/course/tecnicas-computacionais-IA-na-escola/task/148627
+
+Adicionar comentário para a turma...
+
+Atividade: "Aula assíncrona 09/09/2024."
+ROBERTO BATISTA CONRADO postou uma nova atividade: Aula assíncrona 09/09/2024.
+Criado em: 9 de set.9 de set.
+
+Aviso: "Projeto MISSAO."
+ROBERTO BATISTA CONRADO
+Criado em: 3 de set.3 de set.
+Projeto MISSAO.
+
+style.css
+Folha de estilo
+
+script.js
+JavaScript
+
+index.html
+HTML
+
+Adicionar comentário para a turma...
+
+Atividade: "Aula assíncrona 02/09/2024."
+ROBERTO BATISTA CONRADO postou uma nova atividade: Aula assíncrona 02/09/2024.
+Criado em: 2 de set.2 de set.
+Atividade: "Aula assíncrona 19/08/2024."
+ROBERTO BATISTA CONRADO postou uma nova atividade: Aula assíncrona 19/08/2024.
+Criado em: 19 de ago.19 de ago.
+Atividade: "Aula assíncrona 12/08/2024."
+ROBERTO BATISTA CONRADO postou uma nova atividade: Aula assíncrona 12/08/2024.
+Criado em: 12 de ago.12 de ago.
+Atividade: "Aula assíncrona 05/08/2024."
+ROBERTO BATISTA CONRADO postou uma nova atividade: Aula assíncrona 05/08/2024.
+Criado em: 5 de ago.5 de ago.
+Atividade: "Aula assíncrona 01/07/2024."
+ROBERTO BATISTA CONRADO postou uma nova atividade: Aula assíncrona 01/07/2024.
+Criado em: 4 de jul.4 de jul. (editado: 6 de ago.)
+Atividade: "Aula assíncrona 24/06/2024."
+ROBERTO BATISTA CONRADO postou uma nova atividade: Aula assíncrona 24/06/2024.
+Criado em: 26 de jun.26 de jun. (editado: 6 de ago.)
+
+Aviso: "link"
+ROBERTO BATISTA CONRADO
+Criado em: 25 de jun.25 de jun.
+link
+
+TOFAS Programming
+https://pt.programming-testing.com/player/auth/login
+
+Adicionar comentário para a turma...
+
+Atividade: "Aula assíncrona 17/06/2024."
+ROBERTO BATISTA CONRADO postou uma nova atividade: Aula assíncrona 17/06/2024.
+Criado em: 18 de jun.18 de jun.
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: ["No início ficou com medo do que essa tecnologia pode fazer. ",
+                    "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
+                ]
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: ["Quis saber como usar IA no seu dia a dia.",
+                    "Foi atrás de vídeos, artigos e mais informaçõe sobre como utilizar essa tecnologia."
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        alternativas: [
+            {
+                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                afirmacao: ["Conseguiu utilizar a IA para buscar informações úteis.",
+                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
+                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
+                ]
+            },
+            {
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                afirmacao: ["Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
+                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
+                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: ["Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ]
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: ["Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: ["Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
+                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
+                ]
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: ["Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
+                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
+                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        alternativas: [
+            {
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+            },
+            {
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+            }
+        ]
+    },
+];
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+mostraPergunta();
+script.js
+Exibindo script.js…
